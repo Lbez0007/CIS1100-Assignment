ef461d3c9fc407c441b18e58d15bf3fda6eef7ee
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2018-12-24 16:48:13 +0100

    Node

--- linearnodes/Node.java
+++ linearnodes/Node.java
@@ -6,19 +6,11 @@ public class Node
 { 
     public AnyClass obj;
     public Node next;
-    
+
     public Node( AnyClass newObj)
     { 
         obj = newObj;
         next = null;
     }
 
-    /*public void show()
-    { 
-        System.out.println( obj.getData());
-    }
-    
-    public void editNode(){
-        obj.edit();
-    }*/
 }
